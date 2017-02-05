fa0d92ef36f118b32c150ebde86363c6b37f2ec9
Nicholas Law <nick@nicholaslaw.com.au>
2017-02-06 06:55:51 +1100

    Add additional enpoints to users API

--- src/Api/Users.php
+++ src/Api/Users.php
@@ -26,12 +26,51 @@ trait Users
      */
     public function getUser($userIdentifier)
     {
-        if ($this->getApiVersion() >= 5) {
+        if ($this->apiVersionIsGreaterThanV4() && !is_numeric($userIdentifier)) {
+            throw new InvalidIdentifierException('user');
+        }
+
+        return $this->get(sprintf('users/%s', (string) $userIdentifier));
+    }
+
+    /**
+     * Get a user's emotes
+     *
+     * @param string|int $userIdentifier
+     * @param string     $accessToken
+     * @throws InvalidIdentifierException
+     * @return array
+     */
+    public function getUserEmotes($userIdentifier, $accessToken)
+    {
+        if ($this->apiVersionIsGreaterThanV4() && !is_numeric($userIdentifier)) {
+            throw new InvalidIdentifierException('user');
+        }
+
+        return $this->get(sprintf('users/%s/emotes', (string) $userIdentifier), [], $accessToken);
+    }
+
+    /**
+     * Check if a user is subscribed to a channel
+     *
+     * @param string|int $userIdentifier
+     * @param string|int $channelIdentifier
+     * @param string     $accessToken
+     * @throws InvalidIdentifierException
+     * @return array
+     */
+    public function checkUserSubscriptionToChannel($userIdentifier, $channelIdentifier, $accessToken)
+    {
+        if ($this->apiVersionIsGreaterThanV4()) {
             if (!is_numeric($userIdentifier)) {
-                throw new InvalidUserIdentifierException();
+                throw new InvalidIdentifierException('user');
+            }
+
+            if (!is_numeric($channelIdentifier)) {
+                throw new InvalidIdentifierException('channel');
             }
         }
 
-        return $this->get(sprintf('users/%s', (string) $userIdentifier), []);
+        return $this->get(sprintf('users/%s/subscriptions/%s', (string) $userIdentifier, (string) $channelIdentifier), [], $accessToken);
     }
 }
