b70ec8ce4cab9849af22b638ed877ae5ce30b86d
Nicholas Law <nick@nicholaslaw.com.au>
2017-02-11 08:42:08 +1100

    Added getTopVideos method

--- src/Api/Videos.php
+++ src/Api/Videos.php
@@ -3,6 +3,10 @@
 namespace TwitchApi\Api;
 
 use TwitchApi\Exceptions\InvalidIdentifierException;
+use TwitchApi\Exceptions\InvalidLimitException;
+use TwitchApi\Exceptions\InvalidOffsetException;
+use TwitchApi\Exceptions\InvalidTypeException;
+use TwitchApi\Exceptions\UnsupportedOptionException;
 
 trait Videos
 {
@@ -19,5 +23,55 @@ trait Videos
         }
 
         return $this->get(sprintf('videos/%s', $videoIdentifier));
+    }
+
+    /**
+     * Get top videos
+     *
+     * @param int    $limit
+     * @param int    $offset
+     * @param string $gamme
+     * @param string $period
+     * @param string $broadcastType (comma-seperated list)
+     * @throws InvalidLimitException
+     * @throws InvalidOffsetException
+     * @throws InvalidTypeException
+     * @throws UnsupportedOptionException
+     * @return array|json
+     */
+    public function getTopVideos($limit = 10, $offset = 0, $game = null, $period = 'week', $broadcastType = 'highlight')
+    {
+        $validPeriods = ['week', 'month', 'all'];
+
+        if (!$this->isValidLimit($limit)) {
+            throw new InvalidLimitException();
+        }
+
+        if (!$this->isValidOffset($offset)) {
+            throw new InvalidOffsetException();
+        }
+
+        if ($game && !is_string($game)) {
+            throw new InvalidTypeException('game', 'string', gettype($game));
+        }
+
+        if (!in_array($period = strtolower($period), $validPeriods)) {
+            throw new UnsupportedOptionException('period', $validPeriods);
+        }
+
+        $broadcastType = trim($broadcastType, ', ');
+        if (!$this->isValidBroadcastType($broadcastType)) {
+            throw new UnsupportedOptionException('broadcastType', $validBroadcastTypes);
+        }
+
+        $params = [
+            'limit' => intval($limit),
+            'offset' => intval($offset),
+            'game' => $game,
+            'period' => $period,
+            'broadcast_type' => $broadcastType,
+        ];
+
+        return $this->get('videos/top', $params);
     }
 }
