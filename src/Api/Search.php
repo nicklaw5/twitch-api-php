d83455b6484aae0510024736597f9a8401991c7e
Nicholas Law <nick@nicholaslaw.com.au>
2017-02-12 08:35:22 +1100

    Added searchStreams method

--- src/Api/Search.php
+++ src/Api/Search.php
@@ -68,4 +68,49 @@ trait Search
 
         return $this->get('search/games', ['query' => $query]);
     }
+
+    /**
+     * Search for streams by channel description or game name
+     *
+     * @param string  $query
+     * @param int     $limit
+     * @param int     $offset
+     * @param boolean $hls
+     * @throws InvalidTypeException
+     * @throws TwitchApiException
+     * @throws InvalidLimitException
+     * @throws InvalidOffsetException
+     * @return array|json
+     */
+    public function searchStreams($query, $limit = 25, $offset = 0, $hls = null)
+    {
+        if (!is_string($query)) {
+            throw new InvalidTypeException('Query', 'string', gettype($query));
+        }
+
+        if (empty($query)) {
+            throw new TwitchApiException('A \'query\' parameter is required.');
+        }
+
+        if (!$this->isValidLimit($limit)) {
+            throw new InvalidLimitException();
+        }
+
+        if (!$this->isValidOffset($offset)) {
+            throw new InvalidOffsetException();
+        }
+
+        if ($hls !== null && !is_bool($hls)) {
+            throw new InvalidTypeException('HLS', 'boolean', gettype($hls));
+        }
+
+        $params = [
+            'query' => $query,
+            'limit' => intval($limit),
+            'offset' => intval($offset),
+            'hls' => $hls,
+        ];
+
+        return $this->get('search/streams', $params);
+    }
 }
