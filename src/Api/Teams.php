9b322cda9ae6b6945541339c8b8735bb3e908412
Nicholas Law <nick@nicholaslaw.com.au>
2017-02-12 07:19:06 +1100

    Added getTeam method

--- src/Api/Teams.php
+++ src/Api/Teams.php
@@ -4,9 +4,26 @@ namespace TwitchApi\Api;
 
 use TwitchApi\Exceptions\InvalidLimitException;
 use TwitchApi\Exceptions\InvalidOffsetException;
+use TwitchApi\Exceptions\InvalidTypeException;
 
 trait Teams
 {
+    /**
+     * Get a team by name
+     *
+     * @param string $name
+     * @throws InvalidTypeException
+     * @return array|json
+     */
+    public function getTeam($name)
+    {
+        if (!is_string($name)) {
+            throw new InvalidTypeException('Name', 'string', gettype($name));
+        }
+
+        return $this->get(sprintf('teams/%s', $name));
+    }
+
     /**
      * Get all active teams
      *
