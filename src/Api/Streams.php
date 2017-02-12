d32a96a9cef7170b7eb0f4e0aa92ab5fc54a023c
Nicholas Law <nick@nicholaslaw.com.au>
2017-02-13 06:58:49 +1100

    Added getLiveStreams method

--- src/Api/Streams.php
+++ src/Api/Streams.php
@@ -3,7 +3,10 @@
 namespace TwitchApi\Api;
 
 use TwitchApi\Exceptions\InvalidIdentifierException;
+use TwitchApi\Exceptions\InvalidLimitException;
+use TwitchApi\Exceptions\InvalidOffsetException;
 use TwitchApi\Exceptions\InvalidStreamTypeException;
+use TwitchApi\Exceptions\InvalidTypeException;
 
 trait Streams
 {
@@ -29,4 +32,68 @@ trait Streams
         return $this->get(sprintf('streams/%s', $userIdendifier), ['stream_type' => $streamType]);
     }
 
+    /**
+     * Get live streams
+     *
+     * @param string $channel (comma-seperated list)
+     * @param string $game
+     * @param string $language
+     * @param string $streamType
+     * @param int    $limit
+     * @param int    $offset
+     * @throws InvalidTypeException
+     * @throws InvalidIdentifierException
+     * @throws InvalidStreamTypeException
+     * @throws InvalidLimitException
+     * @throws InvalidOffsetException
+     * @return array|json
+     */
+    public function getLiveStreams($channel = null, $game = null, $language = null, $streamType = 'live', $limit = 25, $offset = 0)
+    {
+        if ($channel) {
+            if (!is_string($channel)) {
+                throw new InvalidTypeException('channel', 'string', gettype($channel));
+            }
+
+            $channel = trim($channel, ', ');
+            if ($this->apiVersionIsGreaterThanV4()) {
+                foreach ($channel as $chan) {
+                    if (!is_numeric($chan)) {
+                        throw new InvalidIdentifierException('channel');
+                    }
+                }
+            }
+        }
+
+        if ($game && !is_string($game)) {
+            throw new InvalidTypeException('game', 'string', gettype($game));
+        }
+
+        if ($language && !is_string($language)) {
+            throw new InvalidTypeException('language', 'string', gettype($language));
+        }
+
+        if (!$this->isValidStreamType($streamType)) {
+            throw new InvalidStreamTypeException();
+        }
+
+        if (!$this->isValidLimit($limit)) {
+            throw new InvalidLimitException();
+        }
+
+        if (!$this->isValidOffset($offset)) {
+            throw new InvalidOffsetException();
+        }
+
+        $params = [
+            'channel' => $channel,
+            'game' => $game,
+            'language' => $language,
+            'stream_type' => $streamType,
+            'limit' => intval($limit),
+            'offset' => intval($offset),
+        ];
+
+        return $this->get('streams', $params);
+    }
 }
