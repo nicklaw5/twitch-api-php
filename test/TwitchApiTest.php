46c26a97380c33fdf2956ec6c104a5c843dc3fab
Nicholas Law <nick@nicholaslaw.com.au>
2017-02-22 19:46:35 +1100

    Add inital basic tests

--- test/TwitchApiTest.php
+++ test/TwitchApiTest.php
@@ -5,5 +5,19 @@ use PHPUnit\Framework\TestCase;
 
 class TwitchApiTest extends TestCase
 {
-    //
+    public function testCanCreateBasicClass()
+    {
+        $twitchApi = new TwitchApi(['client_id' => 'CLIENT-ID']);
+        $this->assertInstanceOf(TwitchApi::class, $twitchApi);
+    }
+
+    public function testDefaultClassProperties()
+    {
+        $twitchApi = new TwitchApi(['client_id' => 'CLIENT-ID']);
+        $this->assertEmpty($twitchApi->getScope());
+        $this->assertNotEmpty($twitchApi->getClientId());
+        $this->assertEmpty($twitchApi->getRedirectUri());
+        $this->assertEmpty($twitchApi->getClientSecret());
+        $this->assertEquals($twitchApi->getApiVersion(), $twitchApi->getDefaultApiVersion());
+    }
 }
