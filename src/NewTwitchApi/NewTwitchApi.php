8f291394eb9589bc737778db3efe99b4a8956dff
B3none <ablackham2000@gmail.com>
2019-05-14 15:38:16 +0100

    update main client

--- src/NewTwitchApi/NewTwitchApi.php
+++ src/NewTwitchApi/NewTwitchApi.php
@@ -14,15 +14,47 @@ use NewTwitchApi\Webhooks\WebhooksSubscriptionApi;
 
 class NewTwitchApi
 {
-    private $oauthApi;
-    private $gamesApi;
-    private $streamsApi;
-    private $usersApi;
-    private $webhooksApi;
-    private $webhooksSubscriptionApi;
+    /**
+     * @var OauthApi
+     */
+    protected $oauthApi;
 
-    public function __construct(Client $helixGuzzleClient, string $clientId, string $clientSecret, Client $authGuzzleClient = null)
+    /**
+     * @var GamesApi
+     */
+    protected $gamesApi;
+
+    /**
+     * @var StreamsApi
+     */
+    protected $streamsApi;
+
+    /**
+     * @var UsersApi
+     */
+    protected $usersApi;
+
+    /**
+     * @var WebhooksApi
+     */
+    protected $webhooksApi;
+
+    /**
+     * @var WebhooksSubscriptionApi
+     */
+    protected $webhooksSubscriptionApi;
+
+    /**
+     * NewTwitchApi constructor.
+     *
+     * @param string $clientId
+     * @param string $clientSecret
+     * @param Client|null $authGuzzleClient
+     */
+    public function __construct(string $clientId, string $clientSecret, Client $authGuzzleClient = null)
     {
+        $helixGuzzleClient = new HelixGuzzleClient($clientId);
+
         $this->oauthApi = new OauthApi($clientId, $clientSecret, $authGuzzleClient);
         $this->gamesApi = new GamesApi($helixGuzzleClient);
         $this->streamsApi = new StreamsApi($helixGuzzleClient);
@@ -31,31 +63,61 @@ class NewTwitchApi
         $this->webhooksSubscriptionApi = new WebhooksSubscriptionApi($clientId, $clientSecret, $helixGuzzleClient);
     }
 
+    /**
+     * Oauth Endpoints
+     *
+     * @return OauthApi
+     */
     public function getOauthApi(): OauthApi
     {
         return $this->oauthApi;
     }
 
+    /**
+     * Games Endpoints
+     *
+     * @return GamesApi
+     */
     public function getGamesApi(): GamesApi
     {
         return $this->gamesApi;
     }
 
+    /**
+     * Streams Endpoints
+     *
+     * @return StreamsApi
+     */
     public function getStreamsApi(): StreamsApi
     {
         return $this->streamsApi;
     }
 
+    /**
+     * Users Endpoints
+     *
+     * @return UsersApi
+     */
     public function getUsersApi(): UsersApi
     {
         return $this->usersApi;
     }
 
+    /**
+     * Webhooks Endpoints
+     *
+     * @return WebhooksApi
+     */
     public function getWebhooksApi(): WebhooksApi
     {
         return $this->webhooksApi;
     }
 
+    /**
+     * Webhooks Subscription Endpoints
+     *
+     * @return WebhooksSubscriptionApi
+     */
     public function getWebhooksSubscriptionApi(): WebhooksSubscriptionApi
     {
         return $this->webhooksSubscriptionApi;
