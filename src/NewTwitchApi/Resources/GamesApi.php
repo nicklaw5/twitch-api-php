d7b92f88e7db7bd1284152ceb8fd39ddd99620f7
Brandin Arsenault <brandin@brandinarsenault.com>
2019-07-19 20:16:31 -0300

    Completed Games API and added optional authentication

--- src/NewTwitchApi/Resources/GamesApi.php
+++ src/NewTwitchApi/Resources/GamesApi.php
@@ -12,8 +12,8 @@ class GamesApi extends AbstractResource
     /**
      * @throws GuzzleException
      * @link https://dev.twitch.tv/docs/api/reference/#get-games
-     */
-    public function getGames(array $ids = [], array $names = []): ResponseInterface
+   */
+    public function getGames(array $ids = [], array $names = [], string $bearer = null): ResponseInterface
     {
         $queryParamsMap = [];
         foreach ($ids as $id) {
@@ -23,6 +23,29 @@ class GamesApi extends AbstractResource
             $queryParamsMap[] = ['key' => 'name', 'value' => $name];
         }
 
-        return $this->callApi('games', $queryParamsMap);
+        return $this->callApi('games', $queryParamsMap, $bearer);
+    }
+
+    /**
+     * @throws GuzzleException
+     * @link https://dev.twitch.tv/docs/api/reference/#get-top-games
+   */
+    public function getTopGames(int $first = null, string $before = null, string $after = null, string $bearer = null): ResponseInterface
+    {
+        $queryParamsMap = [];
+
+        if ($first) {
+            $queryParamsMap[] = ['key' => 'first', 'value' => $first];
+        }
+
+        if ($before) {
+            $queryParamsMap[] = ['key' => 'before', 'value' => $before];
+        }
+
+        if ($after) {
+            $queryParamsMap[] = ['key' => 'after', 'value' => $after];
+        }
+
+        return $this->callApi('games/top', $queryParamsMap, $bearer);
     }
 }
