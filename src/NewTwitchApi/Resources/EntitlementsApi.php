7a90a0754c9869ecc04c44df012c8295fbf82398
Max Hayman <maxhayman@maxhayman.co.uk>
2020-07-19 19:39:04 +0100

    +added support for 'entitlements/upload' (#70)
    * +added support for 'entitlements/upload'
    
    * *clean up tabs

---
+++ src/NewTwitchApi/Resources/EntitlementsApi.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace NewTwitchApi\Resources;
+
+use GuzzleHttp\Exception\GuzzleException;
+use Psr\Http\Message\ResponseInterface;
+
+class EntitlementsApi extends AbstractResource
+{
+    /**
+     * @throws GuzzleException
+     */
+    public function getEntitlementGrantsUploadURL(string $bearer, string $manifestId, string $type = "bulk_drops_grant"): ResponseInterface
+    {
+        $queryParamsMap = [];
+        $queryParamsMap[] = ['key' => 'manifest_id', 'value' => $manifestId];
+        $queryParamsMap[] = ['key' => 'type', 'value' => $type];
+        return $this->postApi('entitlements/upload', $bearer, $queryParamsMap);
+    }
+}
