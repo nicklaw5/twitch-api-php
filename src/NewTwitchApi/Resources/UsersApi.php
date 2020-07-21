01ec1f16b5ae8e8ceb18099dba287043757e3c12
Brandin Arsenault <15201621+brandinarsenault@users.noreply.github.com>
2020-07-21 18:20:24 -0300

    Resolve failure with it_should_create_a_follow_without_notifications

--- src/NewTwitchApi/Resources/UsersApi.php
+++ src/NewTwitchApi/Resources/UsersApi.php
@@ -114,7 +114,7 @@ class UsersApi extends AbstractResource
 
         $queryParamsMap[] = ['key' => 'to_id', 'value' => $toId];
 
-        if ($notifications !== null) {
+        if (!is_null($notifications)) {
             $queryParamsMap[] = ['key' => 'allow_notifications', 'value' => $notifications];
         }
 
