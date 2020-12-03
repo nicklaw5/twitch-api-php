a5c6a16283c39ddf1b8d3153611d2a0d00d666ea
Brandin Arsenault <15201621+brandinarsenault@users.noreply.github.com>
2020-12-03 17:52:14 -0400

    Revert CS Fixes on Spec Files

--- spec/NewTwitchApi/NewTwitchApiSpec.php
+++ spec/NewTwitchApi/NewTwitchApiSpec.php
@@ -18,62 +18,62 @@ use PhpSpec\ObjectBehavior;
 
 class NewTwitchApiSpec extends ObjectBehavior
 {
-    public function let(Client $guzzleClient)
+    function let(Client $guzzleClient)
     {
         $this->beConstructedWith($guzzleClient, 'client-id', 'client-secret');
     }
 
-    public function it_should_provide_oauth_api()
+    function it_should_provide_oauth_api()
     {
         $this->getOauthApi()->shouldBeAnInstanceOf(OauthApi::class);
     }
 
-    public function it_should_provide_analytics_api()
+    function it_should_provide_analytics_api()
     {
         $this->getAnalyticsApi()->shouldBeAnInstanceOf(AnalyticsApi::class);
     }
 
-    public function it_should_provide_bits_api()
+    function it_should_provide_bits_api()
     {
         $this->getBitsApi()->shouldBeAnInstanceOf(BitsApi::class);
     }
 
-    public function it_should_provide_channel_points_api()
+    function it_should_provide_channel_points_api()
     {
         $this->getChannelPointsApi()->shouldBeAnInstanceOf(ChannelPointsApi::class);
     }
 
-    public function it_should_provide_entitlements_api()
+    function it_should_provide_entitlements_api()
     {
         $this->getEntitlementsApi()->shouldBeAnInstanceOf(EntitlementsApi::class);
     }
 
-    public function it_should_provide_games_api()
+    function it_should_provide_games_api()
     {
         $this->getGamesApi()->shouldBeAnInstanceOf(GamesApi::class);
     }
 
-    public function it_should_provide_streams_api()
+    function it_should_provide_streams_api()
     {
         $this->getStreamsApi()->shouldBeAnInstanceOf(StreamsApi::class);
     }
 
-    public function it_should_provide_tags_api()
+    function it_should_provide_tags_api()
     {
         $this->getTagsApi()->shouldBeAnInstanceOf(TagsApi::class);
     }
 
-    public function it_should_provide_users_api()
+    function it_should_provide_users_api()
     {
         $this->getUsersApi()->shouldBeAnInstanceOf(UsersApi::class);
     }
 
-    public function it_should_provide_webhooks_api()
+    function it_should_provide_webhooks_api()
     {
         $this->getWebhooksApi()->shouldBeAnInstanceOf(WebhooksApi::class);
     }
 
-    public function it_should_provide_webhooks_subscription_api()
+    function it_should_provide_webhooks_subscription_api()
     {
         $this->getWebhooksSubscriptionApi()->shouldBeAnInstanceOf(WebhooksSubscriptionApi::class);
     }
