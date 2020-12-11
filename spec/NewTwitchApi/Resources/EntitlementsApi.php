1218dab48cd344aef4b296750d23879fb76c82a1
Brandin Arsenault <15201621+brandinarsenault@users.noreply.github.com>
2020-12-11 18:11:13 -0400

    Add EntitlementsApi->getDropsEntitlements

--- spec/NewTwitchApi/Resources/EntitlementsApi.php
+++ spec/NewTwitchApi/Resources/EntitlementsApi.php
@@ -26,4 +26,34 @@ class EntitlementsApiSpec extends ObjectBehavior
         $guzzleClient->send(new Request('POST', 'entitlements/code?user_id=123&code=abc', ['Authorization' => 'Bearer TEST_TOKEN']))->willReturn($response);
         $this->redeemCode('TEST_TOKEN', '123', ['abc'])->shouldBeAnInstanceOf(ResponseInterface::class);
     }
+
+    function it_should_get_drop_entitlements_by_id(Client $guzzleClient, Response $response)
+    {
+        $guzzleClient->send(new Request('GET', 'entitlements/drops?id=abc', ['Authorization' => 'Bearer TEST_TOKEN']))->willReturn($response);
+        $this->getDropsEntitlements('TEST_TOKEN', 'abc')->shouldBeAnInstanceOf(ResponseInterface::class);
+    }
+
+    function it_should_get_drop_entitlements_by_user_id(Client $guzzleClient, Response $response)
+    {
+        $guzzleClient->send(new Request('GET', 'entitlements/drops?user_id=123', ['Authorization' => 'Bearer TEST_TOKEN']))->willReturn($response);
+        $this->getDropsEntitlements('TEST_TOKEN', NULL, '123')->shouldBeAnInstanceOf(ResponseInterface::class);
+    }
+
+    function it_should_get_drop_entitlements_by_user_id_game_id(Client $guzzleClient, Response $response)
+    {
+        $guzzleClient->send(new Request('GET', 'entitlements/drops?user_id=123&game_id=321', ['Authorization' => 'Bearer TEST_TOKEN']))->willReturn($response);
+        $this->getDropsEntitlements('TEST_TOKEN', NULL, '123', '321')->shouldBeAnInstanceOf(ResponseInterface::class);
+    }
+
+    function it_should_get_drop_entitlements_by_game_id(Client $guzzleClient, Response $response)
+    {
+        $guzzleClient->send(new Request('GET', 'entitlements/drops?game_id=321', ['Authorization' => 'Bearer TEST_TOKEN']))->willReturn($response);
+        $this->getDropsEntitlements('TEST_TOKEN', NULL, NULL, '321')->shouldBeAnInstanceOf(ResponseInterface::class);
+    }
+
+    function it_should_get_drop_entitlements_by_with_all(Client $guzzleClient, Response $response)
+    {
+        $guzzleClient->send(new Request('GET', 'entitlements/drops?user_id=123&game_id=321&after=fff&first=100', ['Authorization' => 'Bearer TEST_TOKEN']))->willReturn($response);
+        $this->getDropsEntitlements('TEST_TOKEN', NULL, '123', '321', 'fff', 100)->shouldBeAnInstanceOf(ResponseInterface::class);
+    }
 }
--- src/NewTwitchApi/Resources/EntitlementsApi.php
+++ src/NewTwitchApi/Resources/EntitlementsApi.php
@@ -41,6 +41,37 @@ class EntitlementsApi extends AbstractResource
 
     /**
      * @throws GuzzleException
+     * @link https://dev.twitch.tv/docs/api/reference#get-drops-entitlements
+     */
+    public function getDropsEntitlements(string $bearer, string $id = null, string $userId = null, string $gameId = null, string $after = null, int $first = null): ResponseInterface
+    {
+        $queryParamsMap = [];
+
+        if($id) {
+            $queryParamsMap[] = ['key' => 'id', 'value' => $id];
+        }
+
+        if($userId) {
+            $queryParamsMap[] = ['key' => 'user_id', 'value' => $userId];
+        }
+
+        if($gameId) {
+            $queryParamsMap[] = ['key' => 'game_id', 'value' => $gameId];
+        }
+
+        if($after) {
+            $queryParamsMap[] = ['key' => 'after', 'value' => $after];
+        }
+
+        if($first) {
+            $queryParamsMap[] = ['key' => 'first', 'value' => $first];
+        }
+
+        return $this->getApi('entitlements/drops', $bearer, $queryParamsMap);
+    }
+
+    /**
+     * @throws GuzzleException
      * @link https://dev.twitch.tv/docs/api/reference#redeem-code
      */
     public function redeemCode(string $bearer, int $userId, array $codes = []): ResponseInterface
