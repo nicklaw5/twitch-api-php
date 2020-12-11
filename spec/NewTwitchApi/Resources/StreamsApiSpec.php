80b7a6abbc31e33509c1751529fb61f88e28d2c6
Brandin Arsenault <15201621+brandinarsenault@users.noreply.github.com>
2020-12-11 17:28:18 -0400

    Remove communityIds parameter in getStreams #89

--- spec/NewTwitchApi/Resources/StreamsApiSpec.php
+++ spec/NewTwitchApi/Resources/StreamsApiSpec.php
@@ -66,30 +66,30 @@ class StreamsApiSpec extends ObjectBehavior
     function it_should_get_streams_by_languages(Client $guzzleClient, Response $response)
     {
         $guzzleClient->send(new Request('GET', 'streams?language=en&language=de', ['Authorization' => 'Bearer TEST_TOKEN']))->willReturn($response);
-        $this->getStreams('TEST_TOKEN', [], [], [], [], ['en', 'de'])->shouldBeAnInstanceOf(ResponseInterface::class);
+        $this->getStreams('TEST_TOKEN', [], [], [], ['en', 'de'])->shouldBeAnInstanceOf(ResponseInterface::class);
     }
 
     function it_should_get_streams_page_by_first(Client $guzzleClient, Response $response)
     {
         $guzzleClient->send(new Request('GET', 'streams?first=42', ['Authorization' => 'Bearer TEST_TOKEN']))->willReturn($response);
-        $this->getStreams('TEST_TOKEN', [], [], [], [], [], 42)->shouldBeAnInstanceOf(ResponseInterface::class);
+        $this->getStreams('TEST_TOKEN', [], [], [], [], 42)->shouldBeAnInstanceOf(ResponseInterface::class);
     }
 
     function it_should_get_streams_page_by_before(Client $guzzleClient, Response $response)
     {
         $guzzleClient->send(new Request('GET', 'streams?before=42', ['Authorization' => 'Bearer TEST_TOKEN']))->willReturn($response);
-        $this->getStreams('TEST_TOKEN', [], [], [], [], [], null, 42)->shouldBeAnInstanceOf(ResponseInterface::class);
+        $this->getStreams('TEST_TOKEN', [], [], [], [], null, 42)->shouldBeAnInstanceOf(ResponseInterface::class);
     }
 
     function it_should_get_streams_page_by_after(Client $guzzleClient, Response $response)
     {
         $guzzleClient->send(new Request('GET', 'streams?after=42', ['Authorization' => 'Bearer TEST_TOKEN']))->willReturn($response);
-        $this->getStreams('TEST_TOKEN', [], [], [], [], [], null, null, 42)->shouldBeAnInstanceOf(ResponseInterface::class);
+        $this->getStreams('TEST_TOKEN', [], [], [], [], null, null, 42)->shouldBeAnInstanceOf(ResponseInterface::class);
     }
 
     function it_should_get_streams_by_everything(Client $guzzleClient, Response $response)
     {
         $guzzleClient->send(new Request('GET', 'streams?user_id=12&user_id=34&user_login=twitchuser&user_login=anotheruser&game_id=56&game_id=78&language=en&language=de&first=100&before=200&after=300', ['Authorization' => 'Bearer TEST_TOKEN']))->willReturn($response);
-        $this->getStreams('TEST_TOKEN', ['12', '34'], ['twitchuser', 'anotheruser'], ['56', '78'], [], ['en', 'de'], 100, 200, 300)->shouldBeAnInstanceOf(ResponseInterface::class);
+        $this->getStreams('TEST_TOKEN', ['12', '34'], ['twitchuser', 'anotheruser'], ['56', '78'], ['en', 'de'], 100, 200, 300)->shouldBeAnInstanceOf(ResponseInterface::class);
     }
 }
