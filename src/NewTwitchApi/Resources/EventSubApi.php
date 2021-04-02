0e3c2382a177a57b5d940c32af6fe758708f6581
Thibault Vlacich <thibault.vlacich@gmail.com>
2021-04-02 13:05:10 +0200

    Add more of the EventSub endpoints

--- src/NewTwitchApi/Resources/EventSubApi.php
+++ src/NewTwitchApi/Resources/EventSubApi.php
@@ -22,65 +22,6 @@ class EventSubApi
     }
 
     /**
-     * @link https://dev.twitch.tv/docs/eventsub/eventsub-subscription-types#channelupdate
-     */
-    public function subscribeToChannelUpdate(string $twitchId, string $callback, string $bearer): void
-    {
-        $this->subscribe(
-            'channel.update',
-            '1',
-            ['broadcaster_user_id' => $twitchId],
-            $callback,
-            $bearer
-        ); 
-    }
-
-    /**
-     * @link https://dev.twitch.tv/docs/eventsub/eventsub-subscription-types#streamonline
-     */
-    public function subscribeToStreamOnline(string $twitchId, string $callback, string $bearer): void
-    {
-        $this->subscribeToStream($twitchId, 'online', $callback, $bearer);
-    }
-
-    /**
-     * @link https://dev.twitch.tv/docs/eventsub/eventsub-subscription-types#streamoffline
-     */
-    public function subscribeToStreamOffline(string $twitchId, string $callback, string $bearer): void
-    {
-        $this->subscribeToStream($twitchId, 'offline', $callback, $bearer);
-    }
-
-    /**
-     * @link https://dev.twitch.tv/docs/eventsub/eventsub-subscription-types#stream-subscriptions
-     */
-    private function subscribeToStream(string $twitchId, string $event, string $callback, string $bearer): void
-    {
-        if (!in_array($event, ['online', 'offline'])) {
-            throw new \InvalidArgumentException('Invalid value for stream event type. Accepted values: online,offline.');
-        }
-
-        $this->subscribe(
-            sprintf('stream.%s', $event),
-            '1',
-            ['broadcaster_user_id' => $twitchId],
-            $callback,
-            $bearer
-        );
-    }
-
-    /**
-     * @link https://dev.twitch.tv/docs/eventsub#verify-a-signature
-     */
-    public function verifySignature(string $signature, string $messageId, string $timestamp, string $body): bool
-    {
-        [$hashAlgorithm, $expectedHash] = explode('=', $signature);
-        $generatedHash = hash_hmac($hashAlgorithm, $messageId . $timestamp . $body, $this->secret);
-
-        return $expectedHash === $generatedHash;
-    }
-
-    /**
      * @throws GuzzleException
      * @link https://dev.twitch.tv/docs/eventsub
      */
@@ -107,5 +48,176 @@ class EventSubApi
             'headers' => $headers,
             'body' => json_encode($body),
         ]);
+    }
+
+    /**
+     * @link https://dev.twitch.tv/docs/eventsub/eventsub-subscription-types#channelupdate
+     */
+    public function subscribeToChannelUpdate(string $twitchId, string $callback, string $bearer): void
+    {
+        $this->subscribe(
+            'channel.update',
+            '1',
+            ['broadcaster_user_id' => $twitchId],
+            $callback,
+            $bearer
+        ); 
+    }
+
+    /**
+     * @link https://dev.twitch.tv/docs/eventsub/eventsub-subscription-types#channelfollow
+     */
+    public function subscribeToChannelFollow(string $twitchId, string $callback, string $bearer): void
+    {
+        $this->subscribe(
+            'channel.follow',
+            '1',
+            ['broadcaster_user_id' => $twitchId],
+            $callback,
+            $bearer
+        );
+    }
+
+    /**
+     * @link https://dev.twitch.tv/docs/eventsub/eventsub-subscription-types#channelsubscribe
+     */
+    public function subscribeToChannelSubscribe(string $twitchId, string $callback, string $bearer): void
+    {
+        $this->subscribe(
+            'channel.subscribe',
+            '1',
+            ['broadcaster_user_id' => $twitchId],
+            $callback,
+            $bearer
+        );
+    }
+
+    /**
+     * @link https://dev.twitch.tv/docs/eventsub/eventsub-subscription-types#channelcheer
+     */
+    public function subscribeToChannelCheer(string $twitchId, string $callback, string $bearer): void
+    {
+        $this->subscribe(
+            'channel.cheer',
+            '1',
+            ['broadcaster_user_id' => $twitchId],
+            $callback,
+            $bearer
+        );
+    }
+
+    /**
+     * @link https://dev.twitch.tv/docs/eventsub/eventsub-subscription-types#channelraid
+     */
+    public function subscribeToChannelRaid(string $twitchId, string $callback, string $bearer): void
+    {
+        $this->subscribe(
+            'channel.raid',
+            '1',
+            ['broadcaster_user_id' => $twitchId],
+            $callback,
+            $bearer
+        );
+    }
+
+    /**
+     * @link https://dev.twitch.tv/docs/eventsub/eventsub-subscription-types#channelban
+     */
+    public function subscribeToChannelBan(string $twitchId, string $callback, string $bearer): void
+    {
+        $this->subscribe(
+            'channel.ban',
+            '1',
+            ['broadcaster_user_id' => $twitchId],
+            $callback,
+            $bearer
+        );
+    }
+
+    /**
+     * @link https://dev.twitch.tv/docs/eventsub/eventsub-subscription-types#channelunban
+     */
+    public function subscribeToChannelUnban(string $twitchId, string $callback, string $bearer): void
+    {
+        $this->subscribe(
+            'channel.unban',
+            '1',
+            ['to_broadcaster_user_id' => $twitchId],
+            $callback,
+            $bearer
+        );
+    }
+
+    /**
+     * @link https://dev.twitch.tv/docs/eventsub/eventsub-subscription-types#stream-subscriptions
+     */
+    private function subscribeToStream(string $twitchId, string $event, string $callback, string $bearer): void
+    {
+        if (!in_array($event, ['online', 'offline'])) {
+            throw new \InvalidArgumentException('Invalid value for stream event type. Accepted values: online,offline.');
+        }
+
+        $this->subscribe(
+            sprintf('stream.%s', $event),
+            '1',
+            ['broadcaster_user_id' => $twitchId],
+            $callback,
+            $bearer
+        );
+    }
+
+    /**
+     * @link https://dev.twitch.tv/docs/eventsub/eventsub-subscription-types#streamonline
+     */
+    public function subscribeToStreamOnline(string $twitchId, string $callback, string $bearer): void
+    {
+        $this->subscribeToStream($twitchId, 'online', $callback, $bearer);
+    }
+
+    /**
+     * @link https://dev.twitch.tv/docs/eventsub/eventsub-subscription-types#streamoffline
+     */
+    public function subscribeToStreamOffline(string $twitchId, string $callback, string $bearer): void
+    {
+        $this->subscribeToStream($twitchId, 'offline', $callback, $bearer);
+    }
+
+    /**
+     * @link https://dev.twitch.tv/docs/eventsub/eventsub-subscription-types#userauthorizationrevoke
+     */
+    public function subscribeToUserAuthorizationRevoke(string $callback, string $bearer): void
+    {
+        $this->subscribe(
+            'user.authorization.revoke',
+            '1',
+            ['client_id' => $this->clientId],
+            $callback,
+            $bearer
+        );
+    }
+
+    /**
+     * @link https://dev.twitch.tv/docs/eventsub/eventsub-subscription-types#userupdate
+     */
+    public function subscribeToUserUpdate(string $twitchId, string $callback, string $bearer): void
+    {
+        $this->subscribe(
+            'user.update',
+            '1',
+            ['user_id' => $twitchId],
+            $callback,
+            $bearer
+        );
+    }
+
+    /**
+     * @link https://dev.twitch.tv/docs/eventsub#verify-a-signature
+     */
+    public function verifySignature(string $signature, string $messageId, string $timestamp, string $body): bool
+    {
+        [$hashAlgorithm, $expectedHash] = explode('=', $signature);
+        $generatedHash = hash_hmac($hashAlgorithm, $messageId . $timestamp . $body, $this->secret);
+
+        return $expectedHash === $generatedHash;
     }
 }
