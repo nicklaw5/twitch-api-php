bad9d12b4377cf6b52bdd1a2be399771eea65e69
Brian Zwahr <echosa@echosa.net>
2021-05-26 16:46:48 -0500

    Update AnalyticsApiSpec

--- spec/NewTwitchApi/Resources/AnalyticsApiSpec.php
+++ spec/NewTwitchApi/Resources/AnalyticsApiSpec.php
@@ -5,97 +5,99 @@ namespace spec\NewTwitchApi\Resources;
 use GuzzleHttp\Client;
 use GuzzleHttp\Psr7\Request;
 use GuzzleHttp\Psr7\Response;
+use NewTwitchApi\RequestGenerator;
 use PhpSpec\ObjectBehavior;
 use Psr\Http\Message\ResponseInterface;
 
 class AnalyticsApiSpec extends ObjectBehavior
 {
-    function let(Client $guzzleClient)
+    function let(Client $guzzleClient, RequestGenerator $requestGenerator, Request $request, Response $response)
     {
-        $this->beConstructedWith($guzzleClient);
+        $this->beConstructedWith($guzzleClient, $requestGenerator);
+        $guzzleClient->send($request)->willReturn($response);
     }
 
-    function it_should_get_extension_analytics(Client $guzzleClient, Response $response)
+    function it_should_get_extension_analytics(RequestGenerator $requestGenerator, Request $request)
     {
-        $guzzleClient->send(new Request('GET', 'analytics/extensions', ['Authorization' => 'Bearer TEST_TOKEN']))->willReturn($response);
+        $requestGenerator->generate('GET', 'analytics/extensions', 'TEST_TOKEN', [], [])->willReturn($request);
         $this->getExtensionAnalytics('TEST_TOKEN')->shouldBeAnInstanceOf(ResponseInterface::class);
     }
 
-    function it_should_get_extension_analytics_by_id(Client $guzzleClient, Response $response)
+    function it_should_get_extension_analytics_by_id(RequestGenerator $requestGenerator, Request $request)
     {
-        $guzzleClient->send(new Request('GET', 'analytics/extensions?extension_id=1', ['Authorization' => 'Bearer TEST_TOKEN']))->willReturn($response);
+        $requestGenerator->generate('GET', 'analytics/extensions', 'TEST_TOKEN', [['key' => 'extension_id', 'value' => '1']], [])->willReturn($request);
         $this->getExtensionAnalytics('TEST_TOKEN', '1')->shouldBeAnInstanceOf(ResponseInterface::class);
     }
 
-    function it_should_get_extension_analytics_with_type(Client $guzzleClient, Response $response)
+    function it_should_get_extension_analytics_with_type(RequestGenerator $requestGenerator, Request $request)
     {
-        $guzzleClient->send(new Request('GET', 'analytics/extensions?type=overview_v1', ['Authorization' => 'Bearer TEST_TOKEN']))->willReturn($response);
+        $requestGenerator->generate('GET', 'analytics/extensions', 'TEST_TOKEN', [['key' => 'type', 'value' => 'overview_v1']], [])->willReturn($request);
         $this->getExtensionAnalytics('TEST_TOKEN', null, 'overview_v1')->shouldBeAnInstanceOf(ResponseInterface::class);
     }
 
-    function it_should_get_extension_analytics_with_first(Client $guzzleClient, Response $response)
+    function it_should_get_extension_analytics_with_first(RequestGenerator $requestGenerator, Request $request)
     {
-        $guzzleClient->send(new Request('GET', 'analytics/extensions?first=100', ['Authorization' => 'Bearer TEST_TOKEN']))->willReturn($response);
+        $requestGenerator->generate('GET', 'analytics/extensions', 'TEST_TOKEN', [['key' => 'first', 'value' => '100']], [])->willReturn($request);
         $this->getExtensionAnalytics('TEST_TOKEN', null, null, 100)->shouldBeAnInstanceOf(ResponseInterface::class);
     }
 
-    function it_should_get_extension_analytics_with_after(Client $guzzleClient, Response $response)
+    function it_should_get_extension_analytics_with_after(RequestGenerator $requestGenerator, Request $request)
     {
-        $guzzleClient->send(new Request('GET', 'analytics/extensions?after=abc', ['Authorization' => 'Bearer TEST_TOKEN']))->willReturn($response);
+        $requestGenerator->generate('GET', 'analytics/extensions', 'TEST_TOKEN', [['key' => 'after', 'value' => 'abc']], [])->willReturn($request);
         $this->getExtensionAnalytics('TEST_TOKEN', null, null, null, 'abc')->shouldBeAnInstanceOf(ResponseInterface::class);
     }
 
-    function it_should_get_extension_analytics_with_started_at(Client $guzzleClient, Response $response)
+    function it_should_get_extension_analytics_with_started_at(RequestGenerator $requestGenerator, Request $request)
     {
-        $guzzleClient->send(new Request('GET', 'analytics/extensions?started_at=2020-01-01T00:00:00Z', ['Authorization' => 'Bearer TEST_TOKEN']))->willReturn($response);
+        $requestGenerator->generate('GET', 'analytics/extensions', 'TEST_TOKEN', [['key' => 'started_at', 'value' => '2020-01-01T00:00:00Z']], [])->willReturn($request);
         $this->getExtensionAnalytics('TEST_TOKEN', null, null, null, null, '2020-01-01T00:00:00Z')->shouldBeAnInstanceOf(ResponseInterface::class);
     }
 
-    function it_should_get_extension_analytics_with_ended_at(Client $guzzleClient, Response $response)
+    function it_should_get_extension_analytics_with_ended_at(RequestGenerator $requestGenerator, Request $request)
     {
-        $guzzleClient->send(new Request('GET', 'analytics/extensions?ended_at=2020-01-01T00:00:00Z', ['Authorization' => 'Bearer TEST_TOKEN']))->willReturn($response);
+        $requestGenerator->generate('GET', 'analytics/extensions', 'TEST_TOKEN', [['key' => 'ended_at', 'value' => '2020-01-01T00:00:00Z']], [])->willReturn($request);
         $this->getExtensionAnalytics('TEST_TOKEN', null, null, null, null, null, '2020-01-01T00:00:00Z')->shouldBeAnInstanceOf(ResponseInterface::class);
     }
 
-    function it_should_get_game_analytics(Client $guzzleClient, Response $response)
+    function it_should_get_game_analytics(RequestGenerator $requestGenerator, Request $request)
     {
-        $guzzleClient->send(new Request('GET', 'analytics/games', ['Authorization' => 'Bearer TEST_TOKEN']))->willReturn($response);
+        $requestGenerator->generate('GET', 'analytics/games', 'TEST_TOKEN', [], [])->willReturn($request);
         $this->getGameAnalytics('TEST_TOKEN')->shouldBeAnInstanceOf(ResponseInterface::class);
     }
 
-    function it_should_get_game_analytics_by_id(Client $guzzleClient, Response $response)
+    function it_should_get_game_analytics_by_id(RequestGenerator $requestGenerator, Request $request)
     {
-        $guzzleClient->send(new Request('GET', 'analytics/games?game_id=1', ['Authorization' => 'Bearer TEST_TOKEN']))->willReturn($response);
+        $requestGenerator->generate('GET', 'analytics/games', 'TEST_TOKEN', [['key' => 'game_id', 'value' => '1']], [])->willReturn($request);
         $this->getGameAnalytics('TEST_TOKEN', '1')->shouldBeAnInstanceOf(ResponseInterface::class);
     }
 
-    function it_should_get_game_analytics_with_type(Client $guzzleClient, Response $response)
+    function it_should_get_game_analytics_with_type(RequestGenerator $requestGenerator, Request $request)
     {
-        $guzzleClient->send(new Request('GET', 'analytics/games?type=overview_v1', ['Authorization' => 'Bearer TEST_TOKEN']))->willReturn($response);
+        $requestGenerator->generate('GET', 'analytics/games', 'TEST_TOKEN', [['key' => 'type', 'value' => 'overview_v1']], [])->willReturn($request);
         $this->getGameAnalytics('TEST_TOKEN', null, 'overview_v1')->shouldBeAnInstanceOf(ResponseInterface::class);
     }
 
-    function it_should_get_game_analytics_with_first(Client $guzzleClient, Response $response)
+    function it_should_get_game_analytics_with_first(RequestGenerator $requestGenerator, Request $request)
     {
-        $guzzleClient->send(new Request('GET', 'analytics/games?first=100', ['Authorization' => 'Bearer TEST_TOKEN']))->willReturn($response);
+        $requestGenerator->generate('GET', 'analytics/games', 'TEST_TOKEN', [['key' => 'first', 'value' => '100']], [])->willReturn($request);
         $this->getGameAnalytics('TEST_TOKEN', null, null, 100)->shouldBeAnInstanceOf(ResponseInterface::class);
     }
 
-    function it_should_get_game_analytics_with_after(Client $guzzleClient, Response $response)
+    function it_should_get_game_analytics_with_after(RequestGenerator $requestGenerator, Request $request)
     {
-        $guzzleClient->send(new Request('GET', 'analytics/games?after=abc', ['Authorization' => 'Bearer TEST_TOKEN']))->willReturn($response);
+        $requestGenerator->generate('GET', 'analytics/games', 'TEST_TOKEN', [['key' => 'after', 'value' => 'abc']], [])->willReturn($request);
         $this->getGameAnalytics('TEST_TOKEN', null, null, null, 'abc')->shouldBeAnInstanceOf(ResponseInterface::class);
     }
 
-    function it_should_get_game_analytics_with_started_at(Client $guzzleClient, Response $response)
+    function it_should_get_game_analytics_with_started_at(RequestGenerator $requestGenerator, Request $request)
     {
-        $guzzleClient->send(new Request('GET', 'analytics/games?started_at=2020-01-01T00:00:00Z', ['Authorization' => 'Bearer TEST_TOKEN']))->willReturn($response);
+        $requestGenerator->generate('GET', 'analytics/games', 'TEST_TOKEN', [['key' => 'started_at', 'value' => '2020-01-01T00:00:00Z']], [])->willReturn($request);
         $this->getGameAnalytics('TEST_TOKEN', null, null, null, null, '2020-01-01T00:00:00Z')->shouldBeAnInstanceOf(ResponseInterface::class);
     }
 
-    function it_should_get_game_analytics_with_ended_at(Client $guzzleClient, Response $response)
+    function it_should_get_game_analytics_with_ended_at(RequestGenerator $requestGenerator, Request $request)
     {
-        $guzzleClient->send(new Request('GET', 'analytics/games?ended_at=2020-01-01T00:00:00Z', ['Authorization' => 'Bearer TEST_TOKEN']))->willReturn($response);
+        $requestGenerator->generate('GET', 'analytics/games', 'TEST_TOKEN', [['key' => 'ended_at', 'value' => '2020-01-01T00:00:00Z']], [])->willReturn($request);
         $this->getGameAnalytics('TEST_TOKEN', null, null, null, null, null, '2020-01-01T00:00:00Z')->shouldBeAnInstanceOf(ResponseInterface::class);
     }
 }
