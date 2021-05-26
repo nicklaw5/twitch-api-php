5d014a9858981e1d555a0f69bad84fe65fe5b544
Brandin Arsenault <brandin@brandinarsenault.com>
2021-05-26 19:21:28 -0300

    Update WebhooksApiSpec

--- spec/NewTwitchApi/Resources/WebhooksApiSpec.php
+++ spec/NewTwitchApi/Resources/WebhooksApiSpec.php
@@ -5,19 +5,21 @@ namespace spec\NewTwitchApi\Resources;
 use GuzzleHttp\Client;
 use GuzzleHttp\Psr7\Request;
 use GuzzleHttp\Psr7\Response;
+use NewTwitchApi\RequestGenerator;
 use PhpSpec\ObjectBehavior;
 use Psr\Http\Message\ResponseInterface;
 
 class WebhooksApiSpec extends ObjectBehavior
 {
-    function let(Client $guzzleClient)
+    function let(Client $guzzleClient, RequestGenerator $requestGenerator, Request $request, Response $response)
     {
-        $this->beConstructedWith($guzzleClient);
+        $this->beConstructedWith($guzzleClient, $requestGenerator);
+        $guzzleClient->send($request)->willReturn($response);
     }
 
-    function it_should_get_user_with_access_token_convenience_method(Client $guzzleClient, Response $response)
+    function it_should_get_user_with_access_token_convenience_method(RequestGenerator $requestGenerator, Request $request, Response $response)
     {
-        $guzzleClient->send(new Request('GET', 'webhooks/subscriptions', ['Authorization' => 'Bearer TEST_TOKEN']))->willReturn($response);
-        $this->getWebhookSubscriptions('TEST_TOKEN')->shouldBeAnInstanceOf(ResponseInterface::class);
+        $requestGenerator->generate('GET', 'webhooks/subscriptions', 'TEST_TOKEN', [], [])->willReturn($request);
+        $this->getWebhookSubscriptions('TEST_TOKEN')->shouldBe($response);
     }
 }
