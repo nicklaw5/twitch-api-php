8075e275534f15b7b2a68e0d50d00b2193cb8386
Brian Zwahr <echosa@echosa.net>
2021-05-26 17:03:39 -0500

    Change spec to test response itself, not just the type.

--- spec/NewTwitchApi/Resources/AnalyticsApiSpec.php
+++ spec/NewTwitchApi/Resources/AnalyticsApiSpec.php
@@ -7,7 +7,6 @@ use GuzzleHttp\Psr7\Request;
 use GuzzleHttp\Psr7\Response;
 use NewTwitchApi\RequestGenerator;
 use PhpSpec\ObjectBehavior;
-use Psr\Http\Message\ResponseInterface;
 
 class AnalyticsApiSpec extends ObjectBehavior
 {
@@ -17,87 +16,87 @@ class AnalyticsApiSpec extends ObjectBehavior
         $guzzleClient->send($request)->willReturn($response);
     }
 
-    function it_should_get_extension_analytics(RequestGenerator $requestGenerator, Request $request)
+    function it_should_get_extension_analytics(RequestGenerator $requestGenerator, Request $request, Response $response)
     {
         $requestGenerator->generate('GET', 'analytics/extensions', 'TEST_TOKEN', [], [])->willReturn($request);
-        $this->getExtensionAnalytics('TEST_TOKEN')->shouldBeAnInstanceOf(ResponseInterface::class);
+        $this->getExtensionAnalytics('TEST_TOKEN')->shouldBe($response);
     }
 
-    function it_should_get_extension_analytics_by_id(RequestGenerator $requestGenerator, Request $request)
+    function it_should_get_extension_analytics_by_id(RequestGenerator $requestGenerator, Request $request, Response $response)
     {
         $requestGenerator->generate('GET', 'analytics/extensions', 'TEST_TOKEN', [['key' => 'extension_id', 'value' => '1']], [])->willReturn($request);
-        $this->getExtensionAnalytics('TEST_TOKEN', '1')->shouldBeAnInstanceOf(ResponseInterface::class);
+        $this->getExtensionAnalytics('TEST_TOKEN', '1')->shouldBe($response);
     }
 
-    function it_should_get_extension_analytics_with_type(RequestGenerator $requestGenerator, Request $request)
+    function it_should_get_extension_analytics_with_type(RequestGenerator $requestGenerator, Request $request, Response $response)
     {
         $requestGenerator->generate('GET', 'analytics/extensions', 'TEST_TOKEN', [['key' => 'type', 'value' => 'overview_v1']], [])->willReturn($request);
-        $this->getExtensionAnalytics('TEST_TOKEN', null, 'overview_v1')->shouldBeAnInstanceOf(ResponseInterface::class);
+        $this->getExtensionAnalytics('TEST_TOKEN', null, 'overview_v1')->shouldBe($response);
     }
 
-    function it_should_get_extension_analytics_with_first(RequestGenerator $requestGenerator, Request $request)
+    function it_should_get_extension_analytics_with_first(RequestGenerator $requestGenerator, Request $request, Response $response)
     {
         $requestGenerator->generate('GET', 'analytics/extensions', 'TEST_TOKEN', [['key' => 'first', 'value' => '100']], [])->willReturn($request);
-        $this->getExtensionAnalytics('TEST_TOKEN', null, null, 100)->shouldBeAnInstanceOf(ResponseInterface::class);
+        $this->getExtensionAnalytics('TEST_TOKEN', null, null, 100)->shouldBe($response);
     }
 
-    function it_should_get_extension_analytics_with_after(RequestGenerator $requestGenerator, Request $request)
+    function it_should_get_extension_analytics_with_after(RequestGenerator $requestGenerator, Request $request, Response $response)
     {
         $requestGenerator->generate('GET', 'analytics/extensions', 'TEST_TOKEN', [['key' => 'after', 'value' => 'abc']], [])->willReturn($request);
-        $this->getExtensionAnalytics('TEST_TOKEN', null, null, null, 'abc')->shouldBeAnInstanceOf(ResponseInterface::class);
+        $this->getExtensionAnalytics('TEST_TOKEN', null, null, null, 'abc')->shouldBe($response);
     }
 
-    function it_should_get_extension_analytics_with_started_at(RequestGenerator $requestGenerator, Request $request)
+    function it_should_get_extension_analytics_with_started_at(RequestGenerator $requestGenerator, Request $request, Response $response)
     {
         $requestGenerator->generate('GET', 'analytics/extensions', 'TEST_TOKEN', [['key' => 'started_at', 'value' => '2020-01-01T00:00:00Z']], [])->willReturn($request);
-        $this->getExtensionAnalytics('TEST_TOKEN', null, null, null, null, '2020-01-01T00:00:00Z')->shouldBeAnInstanceOf(ResponseInterface::class);
+        $this->getExtensionAnalytics('TEST_TOKEN', null, null, null, null, '2020-01-01T00:00:00Z')->shouldBe($response);
     }
 
-    function it_should_get_extension_analytics_with_ended_at(RequestGenerator $requestGenerator, Request $request)
+    function it_should_get_extension_analytics_with_ended_at(RequestGenerator $requestGenerator, Request $request, Response $response)
     {
         $requestGenerator->generate('GET', 'analytics/extensions', 'TEST_TOKEN', [['key' => 'ended_at', 'value' => '2020-01-01T00:00:00Z']], [])->willReturn($request);
-        $this->getExtensionAnalytics('TEST_TOKEN', null, null, null, null, null, '2020-01-01T00:00:00Z')->shouldBeAnInstanceOf(ResponseInterface::class);
+        $this->getExtensionAnalytics('TEST_TOKEN', null, null, null, null, null, '2020-01-01T00:00:00Z')->shouldBe($response);
     }
 
-    function it_should_get_game_analytics(RequestGenerator $requestGenerator, Request $request)
+    function it_should_get_game_analytics(RequestGenerator $requestGenerator, Request $request, Response $response)
     {
         $requestGenerator->generate('GET', 'analytics/games', 'TEST_TOKEN', [], [])->willReturn($request);
-        $this->getGameAnalytics('TEST_TOKEN')->shouldBeAnInstanceOf(ResponseInterface::class);
+        $this->getGameAnalytics('TEST_TOKEN')->shouldBe($response);
     }
 
-    function it_should_get_game_analytics_by_id(RequestGenerator $requestGenerator, Request $request)
+    function it_should_get_game_analytics_by_id(RequestGenerator $requestGenerator, Request $request, Response $response)
     {
         $requestGenerator->generate('GET', 'analytics/games', 'TEST_TOKEN', [['key' => 'game_id', 'value' => '1']], [])->willReturn($request);
-        $this->getGameAnalytics('TEST_TOKEN', '1')->shouldBeAnInstanceOf(ResponseInterface::class);
+        $this->getGameAnalytics('TEST_TOKEN', '1')->shouldBe($response);
     }
 
-    function it_should_get_game_analytics_with_type(RequestGenerator $requestGenerator, Request $request)
+    function it_should_get_game_analytics_with_type(RequestGenerator $requestGenerator, Request $request, Response $response)
     {
         $requestGenerator->generate('GET', 'analytics/games', 'TEST_TOKEN', [['key' => 'type', 'value' => 'overview_v1']], [])->willReturn($request);
-        $this->getGameAnalytics('TEST_TOKEN', null, 'overview_v1')->shouldBeAnInstanceOf(ResponseInterface::class);
+        $this->getGameAnalytics('TEST_TOKEN', null, 'overview_v1')->shouldBe($response);
     }
 
-    function it_should_get_game_analytics_with_first(RequestGenerator $requestGenerator, Request $request)
+    function it_should_get_game_analytics_with_first(RequestGenerator $requestGenerator, Request $request, Response $response)
     {
         $requestGenerator->generate('GET', 'analytics/games', 'TEST_TOKEN', [['key' => 'first', 'value' => '100']], [])->willReturn($request);
-        $this->getGameAnalytics('TEST_TOKEN', null, null, 100)->shouldBeAnInstanceOf(ResponseInterface::class);
+        $this->getGameAnalytics('TEST_TOKEN', null, null, 100)->shouldBe($response);
     }
 
-    function it_should_get_game_analytics_with_after(RequestGenerator $requestGenerator, Request $request)
+    function it_should_get_game_analytics_with_after(RequestGenerator $requestGenerator, Request $request, Response $response)
     {
         $requestGenerator->generate('GET', 'analytics/games', 'TEST_TOKEN', [['key' => 'after', 'value' => 'abc']], [])->willReturn($request);
-        $this->getGameAnalytics('TEST_TOKEN', null, null, null, 'abc')->shouldBeAnInstanceOf(ResponseInterface::class);
+        $this->getGameAnalytics('TEST_TOKEN', null, null, null, 'abc')->shouldBe($response);
     }
 
-    function it_should_get_game_analytics_with_started_at(RequestGenerator $requestGenerator, Request $request)
+    function it_should_get_game_analytics_with_started_at(RequestGenerator $requestGenerator, Request $request, Response $response)
     {
         $requestGenerator->generate('GET', 'analytics/games', 'TEST_TOKEN', [['key' => 'started_at', 'value' => '2020-01-01T00:00:00Z']], [])->willReturn($request);
-        $this->getGameAnalytics('TEST_TOKEN', null, null, null, null, '2020-01-01T00:00:00Z')->shouldBeAnInstanceOf(ResponseInterface::class);
+        $this->getGameAnalytics('TEST_TOKEN', null, null, null, null, '2020-01-01T00:00:00Z')->shouldBe($response);
     }
 
-    function it_should_get_game_analytics_with_ended_at(RequestGenerator $requestGenerator, Request $request)
+    function it_should_get_game_analytics_with_ended_at(RequestGenerator $requestGenerator, Request $request, Response $response)
     {
         $requestGenerator->generate('GET', 'analytics/games', 'TEST_TOKEN', [['key' => 'ended_at', 'value' => '2020-01-01T00:00:00Z']], [])->willReturn($request);
-        $this->getGameAnalytics('TEST_TOKEN', null, null, null, null, null, '2020-01-01T00:00:00Z')->shouldBeAnInstanceOf(ResponseInterface::class);
+        $this->getGameAnalytics('TEST_TOKEN', null, null, null, null, null, '2020-01-01T00:00:00Z')->shouldBe($response);
     }
 }
