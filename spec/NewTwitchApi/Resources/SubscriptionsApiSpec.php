c38441d44b2de6ee600c1a8831ae08304d15c5aa
Brandin Arsenault <brandin@brandinarsenault.com>
2021-05-26 22:15:57 -0300

    SubscriptionsApiSpec Fixes for #67

--- spec/NewTwitchApi/Resources/SubscriptionsApiSpec.php
+++ spec/NewTwitchApi/Resources/SubscriptionsApiSpec.php
@@ -17,6 +17,36 @@ class SubscriptionsApiSpec extends ObjectBehavior
         $guzzleClient->send($request)->willReturn($response);
     }
 
+    function it_should_get_broadcaster_subscriptions(RequestGenerator $requestGenerator, Request $request, Response $response)
+    {
+        $requestGenerator->generate('GET', 'subscriptions', 'TEST_TOKEN', [['key' => 'broadcaster_id', 'value' => '123']], [])->willReturn($request);
+        $this->getBroadcasterSubscriptions('TEST_TOKEN', '123')->shouldBe($response);
+    }
+
+    function it_should_get_broadcaster_subscriptions_with_all(RequestGenerator $requestGenerator, Request $request, Response $response)
+    {
+        $requestGenerator->generate('GET', 'subscriptions', 'TEST_TOKEN', [['key' => 'broadcaster_id', 'value' => '123'], ['key' => 'first', 'value' => 100], ['key' => 'after', 'value' => 'abc']], [])->willReturn($request);
+        $this->getBroadcasterSubscriptions('TEST_TOKEN', '123', 100, 'abc')->shouldBe($response);
+    }
+
+    function it_should_get_broadcaster_subscribers(RequestGenerator $requestGenerator, Request $request, Response $response)
+    {
+        $requestGenerator->generate('GET', 'subscriptions', 'TEST_TOKEN', [['key' => 'broadcaster_id', 'value' => '123']], [])->willReturn($request);
+        $this->getBroadcasterSubscribers('TEST_TOKEN', '123')->shouldBe($response);
+    }
+
+    function it_should_get_broadcaster_subscribers_with_id(RequestGenerator $requestGenerator, Request $request, Response $response)
+    {
+        $requestGenerator->generate('GET', 'subscriptions', 'TEST_TOKEN', [['key' => 'broadcaster_id', 'value' => '123'], ['key' => 'user_id', 'value' => '321']], [])->willReturn($request);
+        $this->getBroadcasterSubscribers('TEST_TOKEN', '123', ['321'])->shouldBe($response);
+    }
+
+    function it_should_get_broadcaster_subscribers_with_ids(RequestGenerator $requestGenerator, Request $request, Response $response)
+    {
+        $requestGenerator->generate('GET', 'subscriptions', 'TEST_TOKEN', [['key' => 'broadcaster_id', 'value' => '123'], ['key' => 'user_id', 'value' => '321'], ['key' => 'user_id', 'value' => '456']], [])->willReturn($request);
+        $this->getBroadcasterSubscribers('TEST_TOKEN', '123', ['321', '456'])->shouldBe($response);
+    }
+
     function it_should_check_user_subscriptions(RequestGenerator $requestGenerator, Request $request, Response $response)
     {
         $requestGenerator->generate('GET', 'subscriptions/user', 'TEST_TOKEN', [['key' => 'broadcaster_id', 'value' => '123'], ['key' => 'user_id', 'value' => '456']], [])->willReturn($request);
