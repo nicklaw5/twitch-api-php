8e324b1993ad1290ebf249bbbdf175d57e3c39cd
Brandin Arsenault <brandin@brandinarsenault.com>
2021-05-26 23:10:22 -0300

    Update EntitlementsApiSpec

--- spec/NewTwitchApi/Resources/EntitlementsApiSpec.php
+++ spec/NewTwitchApi/Resources/EntitlementsApiSpec.php
@@ -5,67 +5,80 @@ namespace spec\NewTwitchApi\Resources;
 use GuzzleHttp\Client;
 use GuzzleHttp\Psr7\Request;
 use GuzzleHttp\Psr7\Response;
+use NewTwitchApi\RequestGenerator;
 use PhpSpec\ObjectBehavior;
-use Psr\Http\Message\ResponseInterface;
 
 class EntitlementsApiSpec extends ObjectBehavior
 {
-    function let(Client $guzzleClient)
+    function let(Client $guzzleClient, RequestGenerator $requestGenerator, Request $request, Response $response)
     {
-        $this->beConstructedWith($guzzleClient);
+        $this->beConstructedWith($guzzleClient, $requestGenerator);
+        $guzzleClient->send($request)->willReturn($response);
     }
 
-    function it_should_get_code_status(Client $guzzleClient, Response $response)
+    function it_should_create_entitlement_grants_upload_url(RequestGenerator $requestGenerator, Request $request, Response $response)
     {
-        $guzzleClient->send(new Request('GET', 'entitlements/codes?user_id=123&code=abc', ['Authorization' => 'Bearer TEST_TOKEN']))->willReturn($response);
-        $this->getCodeStatus('TEST_TOKEN', '123', ['abc'])->shouldBeAnInstanceOf(ResponseInterface::class);
+        $requestGenerator->generate('POST', 'entitlements/upload', 'TEST_TOKEN', [['key' => 'manifest_id', 'value' => '123'], ['key' => 'type', 'value' => 'bulk_drops_grant']], [])->willReturn($request);
+        $this->createEntitlementGrantsUploadURL('TEST_TOKEN', '123', 'bulk_drops_grant')->shouldBe($response);
     }
 
-    function it_should_redeem_code(Client $guzzleClient, Response $response)
+    function it_should_create_entitlement_grants_upload_url_shorthand(RequestGenerator $requestGenerator, Request $request, Response $response)
     {
-        $guzzleClient->send(new Request('POST', 'entitlements/code?user_id=123&code=abc', ['Authorization' => 'Bearer TEST_TOKEN']))->willReturn($response);
-        $this->redeemCode('TEST_TOKEN', '123', ['abc'])->shouldBeAnInstanceOf(ResponseInterface::class);
+        $requestGenerator->generate('POST', 'entitlements/upload', 'TEST_TOKEN', [['key' => 'manifest_id', 'value' => '123'], ['key' => 'type', 'value' => 'bulk_drops_grant']], [])->willReturn($request);
+        $this->createEntitlementGrantsUploadURL('TEST_TOKEN', '123')->shouldBe($response);
     }
 
-    function it_should_get_drop_entitlements_by_id(Client $guzzleClient, Response $response)
+    function it_should_get_code_status(RequestGenerator $requestGenerator, Request $request, Response $response)
     {
-        $guzzleClient->send(new Request('GET', 'entitlements/drops?id=abc', ['Authorization' => 'Bearer TEST_TOKEN']))->willReturn($response);
-        $this->getDropsEntitlements('TEST_TOKEN', 'abc')->shouldBeAnInstanceOf(ResponseInterface::class);
+        $requestGenerator->generate('GET', 'entitlements/codes', 'TEST_TOKEN', [['key' => 'user_id', 'value' => '123'], ['key' => 'code', 'value' => 'abc']], [])->willReturn($request);
+        $this->getCodeStatus('TEST_TOKEN', '123', ['abc'])->shouldBe($response);
     }
 
-    function it_should_get_drop_entitlements_by_user_id(Client $guzzleClient, Response $response)
+    function it_should_get_codes_status(RequestGenerator $requestGenerator, Request $request, Response $response)
     {
-        $guzzleClient->send(new Request('GET', 'entitlements/drops?user_id=123', ['Authorization' => 'Bearer TEST_TOKEN']))->willReturn($response);
-        $this->getDropsEntitlements('TEST_TOKEN', NULL, '123')->shouldBeAnInstanceOf(ResponseInterface::class);
+        $requestGenerator->generate('GET', 'entitlements/codes', 'TEST_TOKEN', [['key' => 'user_id', 'value' => '123'], ['key' => 'code', 'value' => 'abc'], ['key' => 'code', 'value' => 'def']], [])->willReturn($request);
+        $this->getCodeStatus('TEST_TOKEN', '123', ['abc', 'def'])->shouldBe($response);
     }
 
-    function it_should_get_drop_entitlements_by_user_id_game_id(Client $guzzleClient, Response $response)
+    function it_should_get_drop_entitlements_by_id(RequestGenerator $requestGenerator, Request $request, Response $response)
     {
-        $guzzleClient->send(new Request('GET', 'entitlements/drops?user_id=123&game_id=321', ['Authorization' => 'Bearer TEST_TOKEN']))->willReturn($response);
-        $this->getDropsEntitlements('TEST_TOKEN', NULL, '123', '321')->shouldBeAnInstanceOf(ResponseInterface::class);
+        $requestGenerator->generate('GET', 'entitlements/drops', 'TEST_TOKEN', [['key' => 'id', 'value' => '123']], [])->willReturn($request);
+        $this->getDropsEntitlements('TEST_TOKEN', '123')->shouldBe($response);
     }
 
-    function it_should_get_drop_entitlements_by_game_id(Client $guzzleClient, Response $response)
+    function it_should_get_drop_entitlements_by_user_id(RequestGenerator $requestGenerator, Request $request, Response $response)
     {
-        $guzzleClient->send(new Request('GET', 'entitlements/drops?game_id=321', ['Authorization' => 'Bearer TEST_TOKEN']))->willReturn($response);
-        $this->getDropsEntitlements('TEST_TOKEN', NULL, NULL, '321')->shouldBeAnInstanceOf(ResponseInterface::class);
+        $requestGenerator->generate('GET', 'entitlements/drops', 'TEST_TOKEN', [['key' => 'user_id', 'value' => '123']], [])->willReturn($request);
+        $this->getDropsEntitlements('TEST_TOKEN', null, '123')->shouldBe($response);
     }
 
-    function it_should_get_drop_entitlements_by_with_all(Client $guzzleClient, Response $response)
+    function it_should_get_drop_entitlements_by_user_id_with_opts(RequestGenerator $requestGenerator, Request $request, Response $response)
     {
-        $guzzleClient->send(new Request('GET', 'entitlements/drops?user_id=123&game_id=321&after=fff&first=100', ['Authorization' => 'Bearer TEST_TOKEN']))->willReturn($response);
-        $this->getDropsEntitlements('TEST_TOKEN', NULL, '123', '321', 'fff', 100)->shouldBeAnInstanceOf(ResponseInterface::class);
+        $requestGenerator->generate('GET', 'entitlements/drops', 'TEST_TOKEN', [['key' => 'user_id', 'value' => '123'], ['key' => 'after', 'value' => 'abc'], ['key' => 'first', 'value' => 100]], [])->willReturn($request);
+        $this->getDropsEntitlements('TEST_TOKEN', null, '123', null, 'abc', 100)->shouldBe($response);
     }
 
-    function it_should_create_entitlement_grants_upload_url(Client $guzzleClient, Response $response)
+    function it_should_get_drop_entitlements_by_game_id(RequestGenerator $requestGenerator, Request $request, Response $response)
     {
-        $guzzleClient->send(new Request('POST', 'entitlements/upload?manifest_id=123&type=bulk_drops_grant', ['Authorization' => 'Bearer TEST_TOKEN']))->willReturn($response);
-        $this->createEntitlementGrantsUploadURL('TEST_TOKEN', '123', 'bulk_drops_grant')->shouldBeAnInstanceOf(ResponseInterface::class);
+        $requestGenerator->generate('GET', 'entitlements/drops', 'TEST_TOKEN', [['key' => 'game_id', 'value' => '123']], [])->willReturn($request);
+        $this->getDropsEntitlements('TEST_TOKEN', null, null, '123')->shouldBe($response);
     }
 
-    function it_should_create_entitlement_grants_upload_url_shorthand(Client $guzzleClient, Response $response)
+    function it_should_get_drop_entitlements_by_game_id_with_opts(RequestGenerator $requestGenerator, Request $request, Response $response)
     {
-        $guzzleClient->send(new Request('POST', 'entitlements/upload?manifest_id=123&type=bulk_drops_grant', ['Authorization' => 'Bearer TEST_TOKEN']))->willReturn($response);
-        $this->createEntitlementGrantsUploadURL('TEST_TOKEN', '123')->shouldBeAnInstanceOf(ResponseInterface::class);
+        $requestGenerator->generate('GET', 'entitlements/drops', 'TEST_TOKEN', [['key' => 'game_id', 'value' => '123'], ['key' => 'after', 'value' => 'abc'], ['key' => 'first', 'value' => 100]], [])->willReturn($request);
+        $this->getDropsEntitlements('TEST_TOKEN', null, null, '123', 'abc', 100)->shouldBe($response);
+    }
+
+    function it_should_redeem_code(RequestGenerator $requestGenerator, Request $request, Response $response)
+    {
+        $requestGenerator->generate('POST', 'entitlements/code', 'TEST_TOKEN', [['key' => 'user_id', 'value' => '123'], ['key' => 'code', 'value' => 'abc']], [])->willReturn($request);
+        $this->redeemCode('TEST_TOKEN', '123', ['abc'])->shouldBe($response);
+    }
+
+    function it_should_redeem_codes(RequestGenerator $requestGenerator, Request $request, Response $response)
+    {
+        $requestGenerator->generate('POST', 'entitlements/code', 'TEST_TOKEN', [['key' => 'user_id', 'value' => '123'], ['key' => 'code', 'value' => 'abc'], ['key' => 'code', 'value' => 'def']], [])->willReturn($request);
+        $this->redeemCode('TEST_TOKEN', '123', ['abc', 'def'])->shouldBe($response);
     }
 }
