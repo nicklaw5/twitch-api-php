f987fede5d947e502bf1b3e8607bb02a190ce897
Brandin Arsenault <brandin@brandinarsenault.com>
2021-05-26 23:43:55 -0300

    Resolve CS Issues

--- src/NewTwitchApi/NewTwitchApi.php
+++ src/NewTwitchApi/NewTwitchApi.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace NewTwitchApi;
 
 use GuzzleHttp\Client;
-use NewTwitchApi\RequestGenerator;
 use NewTwitchApi\Auth\OauthApi;
 use NewTwitchApi\Resources\AnalyticsApi;
 use NewTwitchApi\Resources\BitsApi;
