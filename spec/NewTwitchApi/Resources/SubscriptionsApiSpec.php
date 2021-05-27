9213f5434ea1e5fd1b637dc7ed295d7d952d0dbd
Brandin Arsenault <brandin@brandinarsenault.com>
2021-05-26 22:09:22 -0300

    Update SubscriptionsApiSpec

--- spec/NewTwitchApi/Resources/SubscriptionsApiSpec.php
+++ spec/NewTwitchApi/Resources/SubscriptionsApiSpec.php
@@ -5,19 +5,21 @@ namespace spec\NewTwitchApi\Resources;
 use GuzzleHttp\Client;
 use GuzzleHttp\Psr7\Request;
 use GuzzleHttp\Psr7\Response;
+use NewTwitchApi\RequestGenerator;
 use PhpSpec\ObjectBehavior;
 use Psr\Http\Message\ResponseInterface;
 
 class SubscriptionsApiSpec extends ObjectBehavior
 {
-    function let(Client $guzzleClient)
+    function let(Client $guzzleClient, RequestGenerator $requestGenerator, Request $request, Response $response)
     {
-        $this->beConstructedWith($guzzleClient);
+        $this->beConstructedWith($guzzleClient, $requestGenerator);
+        $guzzleClient->send($request)->willReturn($response);
     }
 
-    function it_should_check_user_subscriptions(Client $guzzleClient, Response $response)
+    function it_should_check_user_subscriptions(RequestGenerator $requestGenerator, Request $request, Response $response)
     {
-        $guzzleClient->send(new Request('GET', 'subscriptions/user?broadcaster_id=123&user_id=456', ['Authorization' => 'Bearer TEST_TOKEN']))->willReturn($response);
-        $this->checkUserSubscription('TEST_TOKEN', '123', '456')->shouldBeAnInstanceOf(ResponseInterface::class);
+        $requestGenerator->generate('GET', 'subscriptions/user', 'TEST_TOKEN', [['key' => 'broadcaster_id', 'value' => '123'], ['key' => 'user_id', 'value' => '456']], [])->willReturn($request);
+        $this->checkUserSubscription('TEST_TOKEN', '123', '456')->shouldBe($response);
     }
 }
