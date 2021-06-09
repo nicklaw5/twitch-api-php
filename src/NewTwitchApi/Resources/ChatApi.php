5c7886d574d0f114a3be9825562a1d327a192650
Brandin Arsenault <brandin@brandinarsenault.com>
2021-06-09 16:47:04 -0300

    Add Emote Endpoints

--- src/NewTwitchApi/Resources/ChatApi.php
+++ src/NewTwitchApi/Resources/ChatApi.php
@@ -11,6 +11,50 @@ class ChatApi extends AbstractResource
 {
     /**
      * @throws GuzzleException
+     * @link https://dev.twitch.tv/docs/api/reference#get-channel-emotes
+     */
+    public function getChannelEmotes(string $bearer, string $broadcasterId): ResponseInterface
+    {
+        $queryParamsMap = [];
+        $queryParamsMap[] = ['key' => 'broadcaster_id', 'value' => $broadcasterId];
+
+        return $this->getApi('chat/emotes', $bearer, $queryParamsMap);
+    }
+
+    /**
+     * @throws GuzzleException
+     * @link https://dev.twitch.tv/docs/api/reference#get-global-emotes
+     */
+    public function getGlobalEmotes(string $bearer): ResponseInterface
+    {
+        return $this->getApi('chat/emotes/global', $bearer);
+    }
+
+    /**
+     * @throws GuzzleException
+     * @link https://dev.twitch.tv/docs/api/reference#get-emote-sets
+     */
+    public function getEmoteSets(string $bearer, array $emoteSetIds = []): ResponseInterface
+    {
+        $queryParamsMap = [];
+
+        foreach ($emoteSetIds as $emoteSetId) {
+            $queryParamsMap[] = ['key' => 'emote_set_id', 'value' => $emoteSetId];
+        }
+
+        return $this->getApi('chat/emotes/set', $bearer, $queryParamsMap);
+    }
+
+    public function getEmoteSet(string $bearer, string $emoteSetId): ResponseInterface
+    {
+        $queryParamsMap = [];
+        $queryParamsMap[] = ['key' => 'emote_set_id', 'value' => $emoteSetId];
+
+        return $this->getApi('chat/emotes/set', $bearer, $queryParamsMap);
+    }
+
+    /**
+     * @throws GuzzleException
      * @link https://dev.twitch.tv/docs/api/reference#get-channel-chat-badges
      */
     public function getChannelChatBadges(string $bearer, string $broadcasterId): ResponseInterface
